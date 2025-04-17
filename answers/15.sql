586e5360d34ed4db6763c23cd5e127894ad6f962
oginoshikibu <oginoshikibu@gmail.com>
2025-04-17 16:41:23 +0900

    fix 15

--- answers/15.sql
+++ answers/15.sql
@@ -4,30 +4,21 @@ SELECT
     c2.name,
     c1.ranking,
     c2.ranking,
-    CASE
-        WHEN NOT EXISTS (
-            SELECT
-                *
-            FROM
-                goals g
-            WHERE
-                g.pairing_id = p.id
-        ) THEN 0
-        ELSE(
-            SELECT
-                COUNT(g.id)
-            FROM
-                goals g
-            WHERE
-                g.pairing_id = p.id
-        )
-    END cnt
+    (
+        SELECT
+            COUNT(g.id)
+        FROM
+            goals g
+        WHERE
+            g.pairing_id = p.id
+    ) cnt
 FROM
     pairings p
     LEFT OUTER JOIN countries c1 ON c1.id = p.my_country_id
     LEFT OUTER JOIN countries c2 ON c2.id = p.enemy_country_id
 WHERE
-    c1.group_name = "C" and c2.group_name = "C"
+    c1.group_name = "C"
+    and c2.group_name = "C"
 ORDER BY
     p.kickoff ASC,
     c1.ranking ASC
